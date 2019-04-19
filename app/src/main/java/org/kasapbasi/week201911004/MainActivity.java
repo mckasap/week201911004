5cd70fe473dd078ca5dd9dcc8a97436044d00dac
mckasap <mckasap@gmail.com>
2019-04-19 17:16:14 +0300

    SPİNNER

--- app/src/main/java/org/kasapbasi/week201911004/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week201911004/MainActivity.java
@@ -2,12 +2,32 @@ package org.kasapbasi.week201911004;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+ProgressBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bar=(ProgressBar) findViewById(R.id.progressBar);
+
     }
+
+public void myClick(View v){
+
+        bar.setVisibility(View.VISIBLE);
+
+
+}
+    public void myClick1(View v){
+
+        bar.setVisibility(View.GONE);
+
+
+    }
+
 }
